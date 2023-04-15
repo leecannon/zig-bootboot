62bd285d8eb850883e7c454c7252104f31d844e3
Lee Cannon <leecannon@leecannon.xyz>
2023-04-15 20:18:06 +0100

    update to zig master

--- build.zig
+++ build.zig
@@ -3,13 +3,14 @@ const std = @import("std");
 pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
-    var main_tests = b.addTest(.{
+    const main_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/bootboot.zig" },
         .optimize = optimize,
     });
+    const run_tests = b.addRunArtifact(main_tests);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&run_tests.step);
 
     b.default_step = test_step;
 }
